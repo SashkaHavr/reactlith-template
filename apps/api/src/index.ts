a135a667a0615ffe46d4862a65687bd85feb4c7f
Oleksandr Havrylov <sashka.havr@gmail.com>
2025-05-18 17:22:55 +0200

    Add api app

---
+++ apps/api/src/index.ts
@@ -0,0 +1,41 @@
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+import { auth } from '@reactlith-template/auth';
+import { envServer } from '@reactlith-template/env/server';
+import { trpcHandler } from '@reactlith-template/trpc';
+
+const app = new Hono();
+
+app.use(
+  '/auth/*',
+  cors({
+    origin: envServer.CORS_ORIGINS,
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
+app.use(
+  '/trpc/*',
+  cors({
+    origin: envServer.CORS_ORIGINS,
+    maxAge: 600,
+    credentials: true,
+  }),
+);
+
+app.on(['POST', 'GET'], '/auth/*', (c) => {
+  return auth.handler(c.req.raw);
+});
+
+app.on(['POST', 'GET'], '/trpc/*', (c) => {
+  return trpcHandler({ request: c.req.raw });
+});
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
