43e89a0f08b2dd38376114dc02c4818bf47b979e
Oleksandr Havrylov <sashka.havr@gmail.com>
2025-05-23 22:20:49 +0200

    Pass trpc options

--- packages/trpc/src/context.ts
+++ packages/trpc/src/context.ts
@@ -1,7 +1,7 @@
 import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 
 export function createContext({ opts }: { opts: FetchCreateContextFnOptions }) {
-  return { request: opts.req };
+  return { request: opts.req, opts: opts };
 }
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
