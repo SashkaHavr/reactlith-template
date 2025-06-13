b2939f38d39337d0bd9abfd57589819ab2c1cdb8
Oleksandr Havrylov <sashka.havr@gmail.com>
2025-06-13 12:11:24 +0200

    Update env

--- apps/web/env.config.ts
+++ apps/web/env.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from '@julr/vite-plugin-validate-env';
 import { z } from 'zod/v4';
 
 export default defineConfig({
-  validator: 'zod',
+  validator: 'standard',
   schema: {
     VITE_API_URL: z
       .union([z.url(), z.literal('')])
+      .optional()
       .default('http://localhost:3000'),
     VITE_API_REVERSE_PROXY_PATH: z
       .string()
