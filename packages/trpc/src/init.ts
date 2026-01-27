f9807b3e5b8df4d5f869b64460d1957504af10a7
Oleksandr Havrylov <47502748+SashkaHavr@users.noreply.github.com>
2026-01-27 16:44:27 +0000

    Move procedures to separate files

--- packages/trpc/src/init.ts
+++ packages/trpc/src/init.ts
@@ -1,10 +1,9 @@
-import { initTRPC, TRPCError } from "@trpc/server";
+import { initTRPC } from "@trpc/server";
 import { getHTTPStatusCodeFromError } from "@trpc/server/http";
 import superjson from "superjson";
 import z, { ZodError } from "zod";
 
 import type { Context } from "#context.ts";
-import type { Permissions } from "@reactlith-template/auth";
 
 import { baseLogger } from "@reactlith-template/utils/logger";
 
@@ -70,72 +69,5 @@ export const publicProcedure = t.procedure.use(async ({ next, path, type, ctx })
 
   return result;
 });
-
-export const protectedProcedure = publicProcedure.use(async ({ ctx, next }) => {
-  const session = await ctx.auth.getSession({
-    headers: ctx.request.headers,
-  });
-  if (!session) {
-    throw new TRPCError({
-      message: "You must authenticate to use this endpoint",
-      code: "UNAUTHORIZED",
-    });
-  }
-
-  ctx.logger.setBindings({
-    user: {
-      id: session.user.id,
-      email: session.user.email,
-      role: session.user.role ?? undefined,
-    },
-  });
-
-  return await next({
-    ctx: {
-      session: session,
-      userId: session.user.id,
-    },
-  });
-});
-
-export function adminProcedure(permissions: Permissions) {
-  return protectedProcedure.use(async ({ ctx, next }) => {
-    const requestPermissionsList = (
-      Object.entries(permissions) as [
-        keyof typeof permissions,
-        NonNullable<(typeof permissions)[keyof typeof permissions]>,
-      ][]
-    )
-      .flatMap(([key, value]) =>
-        value.map((p): `${keyof typeof permissions}.${typeof p}` => `${key}.${p}`),
-      )
-      .flat();
-
-    ctx.logger.setBindings({
-      adminProcedure: {
-        permissionsRequested: requestPermissionsList,
-      },
-    });
-
-    if (requestPermissionsList.length === 0) {
-      throw new TRPCError({
-        message: "No permissions specified for adminProcedure",
-        code: "INTERNAL_SERVER_ERROR",
-      });
-    }
-
-    const hasPermission = await ctx.auth.userHasPermission({
-      body: { userId: ctx.userId, permissions: permissions },
-    });
-    if (!hasPermission.success) {
-      throw new TRPCError({
-        message: "You don't have permissions to access this endpoint",
-        code: "FORBIDDEN",
-      });
-    }
-
-    return await next();
-  });
-}
 
 export const createCallerFactory = t.createCallerFactory;
