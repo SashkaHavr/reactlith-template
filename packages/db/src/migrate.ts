3e48d0fdb9907283eca6f08ea64ee11ef01ea1e8
Oleksandr Havrylov <47502748+SashkaHavr@users.noreply.github.com>
2026-04-02 22:26:10 +0000

    Update drizzle

--- packages/db/src/migrate.ts
+++ packages/db/src/migrate.ts
@@ -1,9 +1,51 @@
+import fs from "fs/promises";
+import os from "os";
+import path from "path";
+import { parseArgs } from "util";
+
 import { migrate } from "drizzle-orm/bun-sql/migrator";
 
 import { db } from "#index.ts";
 
+async function foreachMigrationFileLine(action: (content: string) => string) {
+  await Promise.all(
+    (await fs.readdir("./drizzle", { withFileTypes: true })).map(async (dir) => {
+      if (!dir.isDirectory()) return;
+      const sqlMigrationFile = Bun.file(path.join("./drizzle", dir.name, "migration.sql"));
+      if (!(await sqlMigrationFile.exists())) return;
+      const migration = await sqlMigrationFile.text();
+      await sqlMigrationFile.write(
+        migration
+          .split(os.EOL)
+          .map((s) => action(s))
+          .join(os.EOL),
+      );
+    }),
+  );
+}
+
 async function main() {
+  const { values } = parseArgs({
+    args: Bun.argv,
+    options: {
+      force: {
+        type: "boolean",
+        default: false,
+      },
+    },
+    strict: true,
+    allowPositionals: true,
+  });
+
+  if (values.force) {
+    await foreachMigrationFileLine((s) => `-- ${s}`);
+  }
+
   await migrate(db, { migrationsFolder: "./drizzle" });
+
+  if (values.force) {
+    await foreachMigrationFileLine((s) => s.slice(3));
+  }
 }
 
 await main();
