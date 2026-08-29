3a475a78e63e842e83f69c5e768a74c6d0f146be
Oleksandr Havrylov <47502748+SashkaHavr@users.noreply.github.com>
2026-08-29 15:22:32 +0000

    Update endpoint definition

--- packages/rpc/src/features/numbers/schema.ts
+++ packages/rpc/src/features/numbers/schema.ts
@@ -31,38 +31,40 @@ export const numberFullOutput = Schema.Struct({
   updatedAt: Schema.DateFromString,
 });
 
-const getCountAbove50 = HttpApiEndpoint.get("getCountAbove50", "/count-above-50", {
-  success: Schema.Struct({ count: Schema.Natural }),
-});
-
-const getAll = HttpApiEndpoint.get("getAll", "/", {
-  success: Schema.Struct({ numbers: Schema.Array(numberOutput) }),
-}).middleware(AuthenticationMiddleware);
-const getById = HttpApiEndpoint.get("getById", "/:id", {
-  params: numberIdInput,
-  success: numberFullOutput,
-  error: NumberNotFound,
-}).middleware(AuthenticationMiddleware);
-const addNew = HttpApiEndpoint.post("addNew", "/", {
-  payload: numberInput,
-  success: numberOutput,
-  error: [MaxCountReached, UserNotFound],
-}).middleware(AuthenticationMiddleware);
-const update = HttpApiEndpoint.patch("update", "/:id", {
-  params: numberIdInput,
-  payload: numberUpdateInput,
-  success: numberFullOutput,
-  error: NumberNotFound,
-}).middleware(AuthenticationMiddleware);
-const deleteNumber = HttpApiEndpoint.delete("delete", "/:id", {
-  params: numberIdInput,
-  success: numberIdInput,
-  error: NumberNotFound,
-}).middleware(AuthenticationMiddleware);
-const deleteAll = HttpApiEndpoint.delete("deleteAll", "/", {
-  success: Schema.Null,
-}).middleware(AuthenticationMiddleware);
-
 export class NumbersApi extends HttpApiGroup.make("numbers")
-  .add(getCountAbove50, getAll, getById, addNew, update, deleteNumber, deleteAll)
+  .add(
+    HttpApiEndpoint.get("getAll", "/", {
+      success: Schema.Struct({ numbers: Schema.Array(numberOutput) }),
+    }),
+    HttpApiEndpoint.get("getById", "/:id", {
+      params: numberIdInput,
+      success: numberFullOutput,
+      error: NumberNotFound,
+    }),
+    HttpApiEndpoint.post("addNew", "/", {
+      payload: numberInput,
+      success: numberOutput,
+      error: [MaxCountReached, UserNotFound],
+    }),
+    HttpApiEndpoint.patch("update", "/:id", {
+      params: numberIdInput,
+      payload: numberUpdateInput,
+      success: numberFullOutput,
+      error: NumberNotFound,
+    }),
+    HttpApiEndpoint.delete("delete", "/:id", {
+      params: numberIdInput,
+      success: numberIdInput,
+      error: NumberNotFound,
+    }),
+    HttpApiEndpoint.delete("deleteAll", "/", {
+      success: Schema.Null,
+    }),
+  )
+  .middleware(AuthenticationMiddleware)
+  .add(
+    HttpApiEndpoint.get("getCountAbove50", "/count-above-50", {
+      success: Schema.Struct({ count: Schema.Natural }),
+    }),
+  )
   .prefix("/numbers") {}
