8511d14ba6df0f09e15522b9a59f908ba828f4ce
Oleksandr Havrylov <47502748+SashkaHavr@users.noreply.github.com>
2026-09-21 23:45:37 +0000

    Update error log

--- packages/api/src/middleware/global/layer.ts
+++ packages/api/src/middleware/global/layer.ts
@@ -8,29 +8,32 @@ export const GlobalMiddlewareLive = Layer.effect(
   GlobalMiddleware,
   Effect.gen(function* () {
     const log = yield* StructuredLogger;
+    const logError = Effect.fn(function* (error: unknown) {
+      if (Predicate.isError(error)) {
+        const tag =
+          Predicate.hasProperty(error, "_tag") && Predicate.isString(error._tag)
+            ? error._tag
+            : undefined;
+        yield* log?.error(
+          {
+            message: error.message !== "" ? error.message : (tag ?? ""),
+            name: error.name,
+            cause: error.cause,
+            stack: error.stack,
+          },
+          {
+            _tag: tag,
+          },
+        );
+      }
+    });
 
     return GlobalMiddleware.of((effect, { endpoint, group }) =>
       Effect.gen(function* () {
         yield* log?.set({
           api: { path: `${group.identifier}.${endpoint.identifier}` },
         });
-        return yield* effect.pipe(
-          Effect.tapError((error) =>
-            Effect.gen(function* () {
-              if (Predicate.isError(error)) {
-                yield* log?.error({
-                  message:
-                    Predicate.hasProperty(error, "_tag") && Predicate.isString(error._tag)
-                      ? error._tag
-                      : error.message,
-                  name: error.name,
-                  cause: error.cause,
-                  stack: error.stack,
-                });
-              }
-            }),
-          ),
-        );
+        return yield* effect.pipe(Effect.tapError(logError), Effect.tapDefect(logError));
       }),
     );
   }),
